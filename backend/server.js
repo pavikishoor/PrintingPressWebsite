3196f3af344c94923a70f30ccc1d13a3c00501f4
Pavithrayani <Pavithrayanikuppuswamy@gmail.com>
2025-10-25 19:37:18 +0530

    revert

--- backend/server.js
+++ backend/server.js
@@ -60,7 +60,7 @@ const Quote = mongoose.model('Quote', QuoteSchema);
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    callbackURL: '/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
